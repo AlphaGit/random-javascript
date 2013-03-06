bf38310bc08d3e9e53b33543a2933ca0204db2b7
Alpha <alphagma@gmail.com>
2013-03-05 18:40:10 -0600

    Loop

--- spirograph-canvas/spirograph.js
+++ spirograph-canvas/spirograph.js
@@ -6,16 +6,25 @@ window.Spirograph = function(userOptions) {
 	var configNode;
 
 	var defaultOptions = {
+		fps: 20,
+		angleIncrement: 0.0001
 	};
+
+	var center = {
+		x: null,
+		y: null
+	};
+	var currentAngle = 0;
 
 	options = mergeObjects(defaultOptions, userOptions);
 	if (!options.canvasNode) throw "canvasSelector is a required parameter";
 	canvas = document.querySelector(options.canvasNode);
+	ctx = canvas.getContext("2d");
+	center.x = canvas.width / 2;
+	center.y = canvas.height / 2;
 
 	if (options.configurationNode)
 		configNode = document.querySelector(options.configurationNode);
-
-	ctx = canvas.getContext("2d");
 
 	function mergeObjects(toOverride, overridingWith) {
 		for (var property in overridingWith) {
@@ -24,12 +33,39 @@ window.Spirograph = function(userOptions) {
 		return toOverride;
 	}
 
+	var animationHandle;
 	function start() {
-		alert("//TODO do magic here!");
+		animationHandle = setInterval(animationLoop, 1000/options.fps);
+	};
+	function stop() {
+		clearInterval(animationHandle);
+	}
+
+	function animationLoop() {
+		//TODO get point position
+		//TODO draw it
+		//TODO increment angle
 	};
 
 	// public methods
 	return {
 		start: start
 	};
+};
+
+function Polygon(centerX, centerY, sides, radius) {
+	var center = {
+		x: centerX,
+		y: centerY
+	};
+
+	return getEdgePosition(angle) {
+		if (angle == 0)
+			return {
+				x: center.x,
+				y: center.y - radius
+			};
+
+		//TODO return x and y for each possible angle in this polygon
+	}
 };
