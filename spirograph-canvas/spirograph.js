c748188fbadec467884d1c18124b92ac314871c6
Alpha <alphagma@gmail.com>
2013-03-05 18:01:10 -0600

    Dynamic CSS.

--- spirograph-canvas/spirograph.js
+++ spirograph-canvas/spirograph.js
@@ -1,20 +1,21 @@
 window.Spirograph = function(userOptions) {
 	// private fields
-	var canvas = null;
-	var ctx = null;
-	var options = null;
+	var canvas;
+	var ctx;
+	var options;
+	var configNode;
 
 	var defaultOptions = {
-		fullWidth: true,
-		fullHeight: true,
-		stageHeight: 600,
-		stageWidth: 1024
 	};
 
 	options = mergeObjects(defaultOptions, userOptions);
 	if (!options.canvasNode) throw "canvasSelector is a required parameter";
-	if (options.fullWidth) options.stageWidth = document.documentElement.clientWidth;
-	if (options.fullHeight) options.stageHeight = document.documentElement.clientHeight;
+	canvas = document.querySelector(options.canvasNode);
+
+	if (options.configurationNode)
+		configNode = document.querySelector(options.configurationNode);
+
+	ctx = canvas.getContext("2d");
 
 	function mergeObjects(toOverride, overridingWith) {
 		for (var property in overridingWith) {
@@ -23,19 +24,7 @@ window.Spirograph = function(userOptions) {
 		return toOverride;
 	}
 
-	function initCanvas() {
-		var body = document.getElementsByTagName("body")[0];
-		var height = options.stageWidth;
-		var width = options.stageHeight;
-		canvas = document.createElement("canvas");
-		canvas.width = height;
-		canvas.height = width;
-		body.appendChild(canvas);
-		ctx = canvas.getContext("2d");
-	}
-
 	function start() {
-		initCanvas();
 		alert("//TODO do magic here!");
 	};
 
