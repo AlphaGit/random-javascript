ac5103409d7c139a350c854f0449d55c760301f9
Alpha <alphagma@gmail.com>
2013-04-26 16:25:02 -0500

    Fixed script insertion, used head to overcome same-origin policy.

---
+++ destroy-me/README.md
@@ -0,0 +1,13 @@
+# WHAT'S THIS FOR?
+
+It completely wrecks the page you're in. Slowly.
+
+# WHY?
+
+Because we can-can-can!
+
+# SOUNDS GREAT, LET ME DO THAT
+
+Grab [this link][1]) to your bookmark bar and hit it on any page. Have fun.
+
+[1]: javascript:var%20s=document.createElement('script');s.src='https://raw.github.com/AlphaGit/random-javascript/master/destroy-me/destroyme.js';document.getElementsByTagName('head')[0].appendChild(s);
--- destroy-me/bookmarklet.js
+++ destroy-me/bookmarklet.js
@@ -1,4 +1,7 @@
-//bookmarklet code
+// bookmarklet code
 var s = document.createElement('script');
-s.src = 'destroyme.js';
+s.src = 'https://raw.github.com/AlphaGit/random-javascript/master/destroy-me/destroyme.js';
 document.getElementsByTagName('body')[0].appendChild(s);
+
+// minified
+javascript:var%20s=document.createElement('script');s.src='https://raw.github.com/AlphaGit/random-javascript/master/destroy-me/destroyme.js';document.getElementsByTagName('head')[0].appendChild(s);
