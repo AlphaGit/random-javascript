37144e1a3b4b5a46ff49f8cfcc9b409bbca47c72
Alpha <alphagma@gmail.com>
2013-08-05 19:57:11 -0500

    Create jqueryfy.js

---
+++ jqueryfy/jqueryfy.js
@@ -0,0 +1,1 @@
+javascript:if(!window.jQuery||confirm('Overwrite\x20current\x20version?\x20v'+jQuery.fn.jquery))(function(d,s){s=d.createElement('script');s.src='https://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js';(d.head||d.documentElement).appendChild(s)})(document);
